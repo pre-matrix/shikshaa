290f326c0e1a44f0e71b8519e9f81b9919309360
pre-matrix <141252251+pre-matrix@users.noreply.github.com>
2023-11-02 18:14:34 +0530

    removed budget in angular.js to stop error while building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from 'src/app/material.module';
+import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
